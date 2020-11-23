36f0aeaab831f20115c73ac20703ef9c111b8519
fabriciored <fabriciojlsantos@gmail.com>
2020-11-23 19:55:16 -0300

    feat: add mysql database connection

--- src/routes/news.js
+++ src/routes/news.js
@@ -1,5 +1,18 @@
 module.exports = (app) => {
-app.get('/news', (req, res) => {
-    res.render('news/newsPortal')
-})
-}
+  app.get("/news", (req, res) => {
+    const mysql = require("mysql");
+
+    const connection = mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "pass",
+      database: "portal_news",
+    });
+
+    connection.query("select * from news", (err, result) => {
+        res.render("news/newsPortal", {news: result});
+    });
+
+    // 
+  });
+};
