6bd3332d20212ec71b90ebee6c017ce6320b7342
fabriciored <fabriciojlsantos@gmail.com>
2020-11-23 20:01:48 -0300

    refactor: separate db connection into a module

--- src/routes/news.js
+++ src/routes/news.js
@@ -1,13 +1,9 @@
+const dbConnection = require('../config/dbConnection');
+
 module.exports = (app) => {
   app.get("/news", (req, res) => {
-    const mysql = require("mysql");
-
-    const connection = mysql.createConnection({
-      host: "localhost",
-      user: "root",
-      password: "pass",
-      database: "portal_news",
-    });
+    
+    const connection = dbConnection();
 
     connection.query("select * from news", (err, result) => {
         res.render("news/news", {news: result});
